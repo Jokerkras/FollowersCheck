ae836514da8c5d9c081dfa53b1b0a166cbc09d90
xcode <xcode@c1r009n03.amm.vsu.ru>
2017-11-25 16:40:18 +0300

    Added ViewController with users

---
+++ FollowersCheck/FollowersTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  FollowersTableViewController.swift
+//  FollowersCheck
+//
+//  Created by xcode on 25.11.2017.
+//  Copyright © 2017 PMUGroup. All rights reserved.
+//
+
+import Foundation
---
+++ FollowersCheck/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  FollowersCheck
+//
+//  Created by xcode on 25.11.2017.
+//  Copyright © 2017 PMUGroup. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
